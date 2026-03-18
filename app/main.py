5bf9b0cd8ebda20a82aab771f8255596a11a940e
Navaneet J <navaneetj001@gmail.com>
2026-03-18 22:56:29 +0530

    Added CORS support for testing

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from app.api.v1.router import api_router
 from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI(
     title="Medication Reconciliation Service",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all (dev only)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix="/api/v1")
